7ec4afaf180dc68bfb9a2d1f641724a6185ac067
Sergio <kurisergio777777@gmail.com>
2022-03-22 19:41:37 -0600

    commit tree 9e1c048ef6efbf892942edd8e4c622153c4d3194

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+
+    return a + b;
+};
+
+function subtract(a, b) {
+    return (a - b)
+};
+
+function multiply(a, b){
+    return (a * b)
+};
+
+function divide (a, b){
+    return (a/b)
+};
+
+
+function increment(n){
+
+    return (n += 1);
+
+}
+
+function decrement(n){
+    return (n -= 1)
+    
+    }
+function makeInt (string){
+    parseInt("string")
+    return (string);
+
+}
+
+function preserveDecimal (string){
+    parseFloat(string)
+    return (string)
+}
+
